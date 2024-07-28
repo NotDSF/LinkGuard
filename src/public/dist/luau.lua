3071043258b9bfd2d5ad142821f2dc4aa3a830ec
dsf <67711164+NotDSF@users.noreply.github.com>
2024-07-28 15:28:32 +0100

    Update luau.lua

--- src/public/dist/luau.lua
+++ src/public/dist/luau.lua
@@ -9,7 +9,11 @@ function linkguard:ValidateLicense(project, key)
     assert(type(project) == "string", "invalid argument #1 to 'ValidateLicense' (string expected)");
     assert(type(key) == "string", "invalid argument #2 to 'ValidateLicense' (string expected)");
 
-    local response = game:HttpGet(format("https://linkguard.cc/v1/project/%s/licenses/%s", project, key));
+    local ok, response = pcall(game.HttpGet, game, format("https://linkguard.cc/v1/project/%s/licenses/%s", project, key));
+    if not ok then
+        return false;
+    end;
+
     local ok, json = pcall(HttpService.JSONDecode, HttpService, response);
     if ok then
         return json.valid;
